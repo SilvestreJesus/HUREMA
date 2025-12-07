f7e65a55b0619d45683b8d8f26a0e756251b209a
silvestre <22690406@tecvalles.mx>
2025-12-07 16:02:59 -0600

    dos

--- view/main/index.php
+++ view/main/index.php
@@ -5,20 +5,15 @@
 <body>
 <?php include '../layouts/modules/alertas.php'; ?>
     <article class="landing-container">
-        <h1 class="verdefast">Bienvenido a <span class="verde">Verde</span><span class="fast">Fast</span></h1>
+        
         <div class="landing-desktop">
             <section class="dato">
-                <div class="iconos">
-                    <span class="material-symbols-outlined">nature</span>
-                    <span class="material-symbols-outlined">bolt</span>
-                    <span class="material-symbols-outlined">speed</span>
-                </div>
-                <p>VerdeFast es un sistema automatizado que utiliza pulsos mioeléctricos de baja intensidad para acelerar el crecimiento de cultivos, reduciendo hasta un 40% los costos de insumos agrícolas tradicionales.</p>
+                <h1 class="verdefast">Bienvenido a <span class="verde">HUREMA</span></h1>
             </section>
-            <p class="pruebalo-ya">¿Ya lo probaste?</p>
+            <p class="pruebalo-ya">¿Tipo de Discapacidad?</p>
             <div class="botones">
-                <a href="/view/form/login.php" class="boton">Inicia sesión</a>
-                <a href="/view/form/registrar_usuario.php?origen=index" class="boton-secundario">Regístrate</a>
+                <a href="/view/form/login.php" class="boton">Ciego</a>
+                <a href="/view/form/registrar_usuario.php?origen=index" class="boton-secundario">Sordo</a>
             </div>
         </div>
         <div class="landing-bg"></div>
